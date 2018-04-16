ed7e084f5bc6e3395b515f92b3e7071652cbd695
Johan Wållgren <johan.g.wallgren@gmail.com>
2018-04-16 21:11:04 +0200

    Delete packagesNeeded.R

--- packagesNeeded.R
+++
@@ -1,9 +0,0 @@
-# Run to install nessesary packages
-# By Johan Wållgren 201804
-
-install.packages('XML')
-install.packages('tidyverse')
-install.packages('tidytext')
-install.packages('tm')
-install.packages('wordcloud')
-install.packages('RColorBrewer')
