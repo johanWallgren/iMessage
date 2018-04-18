cd3a679e2cbd0db9f03dd5ff8edfa69692649e7a
Johan Wållgren <johan.g.wallgren@gmail.com>
2018-04-18 12:19:14 +0200

    Update server.R

--- MessApp/server.R
+++ MessApp/server.R
@@ -22,9 +22,7 @@ server <- function(input, output) {
   
   ################################################
   # Using inputs on tab Statistics
-  
-  
-  
+
   output$selectedX <- renderText({
     paste(input$xValue)
   })
@@ -32,8 +30,7 @@ server <- function(input, output) {
   output$selectedFill <- renderText({
     paste(input$fillValue)
   })
-  
-  
+    
   ################################################
   # Possilbe selections:
   # X: Weekdays Years Hours People
@@ -42,10 +39,55 @@ server <- function(input, output) {
   
   ################################################
   # Reactive function returning data to plot
-  datasetInput <- reactive({
+  statisticsInput <- reactive({
+
+    filtMess <-
+      filter(
+        mess,
+        year >= input$years[1] &
+          year <= input$years[2] &
+          weekday %in% input$weekdays &
+          person %in% input$person &
+          hour >= input$hours[1] &
+          hour <= input$hours[2]
+      )
     
-    print(input$weekdays)
-    print(input$person)
+    factorOrder <-
+      c('Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday')
+    
+    filtMess$weekday <-
+      ordered(filtMess$weekday, levels = factorOrder)
+    
+    plotData <- as_tibble()
+    
+    for (i in 1:nrow(unique(filtMess[input$fillValue]))) {
+      # fill
+      
+      temp <-
+        filter(filtMess, filtMess[input$fillValue] == as.character(unique(filtMess[input$fillValue][[i, 1]]))) %>%
+        group_by_(as.name(input$xValue)) %>% # x-axis
+        summarize(numberOfMessages = n())
+      
+      temp[input$fillValue] <-
+        as.character(unique(mess[input$fillValue])[i, ][[1]])
+      
+      plotData <- bind_rows(plotData, temp)
+    }
+    rm(temp)
+    plotData
+    
+  })
+  
+  
+  ################################################
+  # Reactive function returning data to plot
+  wordcloudInput <- reactive({
     
     filtMess <-
       filter(
@@ -66,27 +108,11 @@ server <- function(input, output) {
         'Friday',
         'Saturday',
         'Sunday')
+    
     filtMess$weekday <-
       ordered(filtMess$weekday, levels = factorOrder)
     
-    plotData <- as_tibble()
-    
-    for (i in 1:nrow(unique(filtMess[input$fillValue]))) {
-      # fill
-      
-      temp <-
-        filter(filtMess, filtMess[input$fillValue] == as.character(unique(filtMess[input$fillValue][[i, 1]]))) %>%
-        group_by_(as.name(input$xValue)) %>% # x-axis
-        summarize(numberOfMessages = n())
-      
-      temp[input$fillValue] <-
-        as.character(unique(mess[input$fillValue])[i, ][[1]])
-      
-      plotData <- bind_rows(plotData, temp)
-    }
-    print((plotData))
-    rm(temp)
-    plotData
+    filtMess
     
   })
   
@@ -94,7 +120,8 @@ server <- function(input, output) {
   # returning interactive plot
   
   output$interactiveMessagePlot <- renderPlot({
-    dataToPlot <- datasetInput()
+    
+    dataToPlot <- statisticsInput()
     
     yValue = 'numberOfMessages'
     
@@ -104,5 +131,45 @@ server <- function(input, output) {
     interactiveMessagePlot + geom_bar(stat = 'identity') +
       theme_bw()
   })
+  
+  ################################################
+  # returning interactive plot
+  
+  output$wordcloudPlot <- renderPlot({
+    
+    dataToPlot <- wordcloudInput()
+    
+    mainLanguage <- input$language
 
+    # Tibble with one word per row
+    words <- unnest_tokens(dataToPlot, word, text)
+
+    # Tibble with stop words to be removed
+    stop_words <- as_tibble(stopwords(mainLanguage))
+    
+    # Remove all numbers as well
+    stop_numbers <- as_tibble(as.character(seq(1:1e6)))
+    
+    stop_words <- bind_rows(stop_words, stop_numbers) %>%
+      rename(word = value)
+    
+    # Removing stop words
+    words <- words %>%
+      anti_join(stop_words)
+    
+    # Calculating frequenzy of words
+    topWords <- count(words, word, sort = TRUE)
+    
+    # Generating word cloud for all messages
+    wordcloud(
+      words = topWords$word,
+      freq = topWords$n,
+      scale=c(4,0.5),
+      min.freq = 10,
+      max.words = 175,
+      random.order = FALSE,
+      rot.per = 0.35,
+      colors = brewer.pal(9, "Set1")
+    )
+  })
 }
